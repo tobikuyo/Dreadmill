8321709f01a3a81b432dd3ef17e8088e818ae697
Tobi Tselios <tobi.tselios@gmail.com>
2020-07-23 13:54:59 +0100

    Initial storyboard and RunViewController setup

---
+++ Dreadmill/Controllers/RunViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RunViewController.swift
+//  Dreadmill
+//
+//  Created by Tobi Kuyoro on 23/07/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class RunViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
