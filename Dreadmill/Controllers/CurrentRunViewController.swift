6f8327814006133d5c6d801e3482b0b7ee6845d2
Tobi Tselios <tobi.tselios@gmail.com>
2020-07-23 21:07:51 +0100

    Initial CurrentRunViewController setup with distance label updating

--- Dreadmill/Controllers/CurrentRunViewController.swift
+++ Dreadmill/Controllers/CurrentRunViewController.swift
@@ -7,24 +7,74 @@
 //
 
 import UIKit
+import MapKit
 
-class CurrentRunViewController: UIViewController {
+class CurrentRunViewController: LocationViewController {
+
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var paceLabel: UILabel!
+    @IBOutlet var distanceLabel: UILabel!
+    @IBOutlet var pauseButton: CircularButton!
+
+    // MARK: - Properties
+
+    private var startLocation: CLLocation!
+    private var lastLocation: CLLocation!
+    private var runDistance = 0.0
+
+    // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        startRun()
     }
-    */
 
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+
+    // MARK: - Methods
+
+    private func startRun() {
+        manager?.startUpdatingLocation()
+        manager?.distanceFilter = 10
+        manager?.delegate = self
+    }
+
+    private func endRun() {
+        manager?.stopUpdatingLocation()
+    }
+
+    // MARK: - IBActions
+
+    @IBAction func stopButtonTapped(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+
+
+    @IBAction func pauseButtonTapped(_ sender: Any) {
+    }
+}
+
+extension CurrentRunViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            checkAuthStatus()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if startLocation == nil {
+            startLocation = locations.first
+        } else if let location = locations.last {
+            runDistance += lastLocation.distance(from: location)
+            distanceLabel.text = runDistance.description
+        }
+
+        lastLocation = locations.last
+    }
 }
