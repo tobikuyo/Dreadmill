523ba796da936c721cc00ceff6a66a2070bf760f
Tobi Tselios <tobi.tselios@gmail.com>
2020-07-23 13:24:59 +0100

    Initial commit

---
+++ Dreadmill/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Dreadmill
+//
+//  Created by Tobi Kuyoro on 23/07/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dreadmill/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Dreadmill
+//
+//  Created by Tobi Kuyoro on 23/07/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
