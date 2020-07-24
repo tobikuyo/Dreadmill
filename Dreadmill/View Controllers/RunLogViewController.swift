261684fc15c4dcdeb5a15826291ae2b3f7ca16ef
Tobi Tselios <tobi.tselios@gmail.com>
2020-07-24 16:45:35 +0100

    Displaying log details for each run

--- Dreadmill/View Controllers/RunLogViewController.swift
+++ Dreadmill/View Controllers/RunLogViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class RunLogViewController: UIViewController {
+class RunLogViewController: InitialViewController {
+
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
 
+extension RunLogViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return realmController.getAllRuns()?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard
+            let cell = tableView.dequeueReusableCell(withIdentifier: Identifier.logCell, for: indexPath) as? LogTableViewCell,
+            let runs = realmController.getAllRuns() else { return UITableViewCell() }
+
+        let run = runs[indexPath.row]
+        cell.run = run
+        return cell
+    }
+}
