020c71b7022d580a80997dd7390a9eaca3cf644d
Tobi Tselios <tobi.tselios@gmail.com>
2020-07-24 16:36:57 +0100

    Extension on Date

--- Dreadmill/Utilities/Extensions.swift
+++ Dreadmill/Utilities/Extensions.swift
@@ -30,3 +30,11 @@ extension Int {
         }
     }
 }
+
+extension Date {
+    func getString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
