cf020342acf3bf0ad846ff39052dd58bb7bfe2d6
Tobi Tselios <tobi.tselios@gmail.com>
2020-07-24 18:31:59 +0100

    Polyline is displaying in MapView

--- Dreadmill/View Controllers/RunViewController.swift
+++ Dreadmill/View Controllers/RunViewController.swift
@@ -26,9 +26,37 @@ class RunViewController: LocationViewController {
         manager?.startUpdatingLocation()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupMapView()
+    }
+
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         manager?.stopUpdatingLocation()
+    }
+
+    private func setupMapView() {
+        guard let overlay = addLastRun() else { return }
+
+        if mapView.overlays.count > 0 {
+            mapView.removeOverlays(mapView.overlays)
+        }
+
+        mapView.addOverlay(overlay)
+    }
+
+    private func addLastRun() -> MKPolyline? {
+        guard let lastRun = realmController.getAllRuns()?.first else { return nil }
+
+        var coordinates: [CLLocationCoordinate2D] = []
+
+        for location in lastRun.locations {
+            let coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+            coordinates.append(coordinate)
+        }
+
+        return MKPolyline(coordinates: coordinates, count: lastRun.locations.count)
     }
 }
 
@@ -39,5 +67,13 @@ extension RunViewController: CLLocationManagerDelegate {
             mapView.showsUserLocation = true
             mapView.userTrackingMode = .follow
         }
+    }
+
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let polyLine = overlay as! MKPolyline
+        let renderer = MKPolylineRenderer(polyline: polyLine)
+        renderer.strokeColor = .systemBlue
+        renderer.lineWidth = 6
+        return renderer
     }
 }
